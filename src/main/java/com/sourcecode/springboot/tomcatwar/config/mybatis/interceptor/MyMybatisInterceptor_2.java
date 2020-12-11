2b32842197a18b346485ba3f6861424a9581038b
wangwei <530220264@qq.com>
2020-12-11 17:56:15 +0800

    MyBatis插件

---
+++ src/main/java/com/sourcecode/springboot/tomcatwar/config/mybatis/interceptor/MyMybatisInterceptor_2.java
@@ -0,0 +1,66 @@
+package com.sourcecode.springboot.tomcatwar.config.mybatis.interceptor;
+
+import org.apache.ibatis.cache.CacheKey;
+import org.apache.ibatis.executor.Executor;
+import org.apache.ibatis.mapping.BoundSql;
+import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.plugin.*;
+import org.apache.ibatis.session.ResultHandler;
+import org.apache.ibatis.session.RowBounds;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Properties;
+
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@Component
+@Intercepts({
+        @Signature(
+                type = Executor.class,
+                method = "query",
+                args = {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class, CacheKey.class, BoundSql.class}),
+        @Signature(
+                type = Executor.class,
+                method = "query",
+                args = {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class}
+        )
+})
+public class MyMybatisInterceptor_2 implements Interceptor {
+
+    public MyMybatisInterceptor_2(){}
+
+    @Override
+    public Object intercept(Invocation invocation) throws Throwable {
+        System.err.println("2_______________111111111111111");
+        Object[] args = invocation.getArgs();
+        MappedStatement ms = (MappedStatement)args[0];
+        Object parameter = args[1];
+        RowBounds rowBounds = (RowBounds)args[2];
+        ResultHandler resultHandler = (ResultHandler)args[3];
+        Executor executor = (Executor)invocation.getTarget();
+        CacheKey cacheKey;
+        BoundSql boundSql;
+        if (args.length == 4) {
+            boundSql = ms.getBoundSql(parameter);
+            cacheKey = executor.createCacheKey(ms, parameter, rowBounds, boundSql);
+        } else {
+            cacheKey = (CacheKey)args[4];
+            boundSql = (BoundSql)args[5];
+        }
+        List<Object> query = executor.query(ms, parameter, rowBounds, resultHandler, cacheKey, boundSql);
+        System.err.println("2_______________222222222222222");
+        return query;
+    }
+
+    @Override
+    public Object plugin(Object target) {
+        return Plugin.wrap(target, this);
+    }
+
+    @Override
+    public void setProperties(Properties properties) {
+
+    }
+}
